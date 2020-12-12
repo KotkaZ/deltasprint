2f4d88729e85258b04576e8caea3eefe76be5cc6
Karl Rapur <rapurkarl@gmail.com>
2020-12-12 21:28:43 +0200

    Small changes to views.

--- src/views/Competition.vue
+++ src/views/Competition.vue
@@ -1,13 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <ProgressBar></ProgressBar>
+    <ScoreTable></ScoreTable>
+    <ExerciseDesc></ExerciseDesc>
+    <ExcerciseStn></ExcerciseStn>
+  </div>
 </template>
 
 <script>
+import ScoreTable from '../components/ScoreTable.vue'
+import ProgressBar from '../components/ProgressBar.vue'
+import ExerciseDesc from '../components/ExerciseDesc.vue'
+import ExcerciseStn from '../components/ExerciseStn.vue'
 export default {
+  name:"Competition",
+  components: [ScoreTable, ProgressBar, ExerciseDesc, ExcerciseStn]
 
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,9 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <UserResult></UserResult>>
+  </div>
 </template>
 
 <script>
+import UserResult from '../components/UserResult.vue'
 export default {
+  name: "Results",
+  components: [UserResult]
 
 }
 </script>
