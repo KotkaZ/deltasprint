c9736d12f6a4ab5572055e8f52e88ec3ba6ce214
Karl Rapur <rapurkarl@gmail.com>
2020-12-12 22:14:29 +0200

    Started with building vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,33 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import CurrentStudent from '../models/CurrentStudent.js';
+import axios from 'axios';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        currentStudent: null,
+        currentCompetition: null,
+        currentTask: null,
+        availableCompetitions: [],
+    },
+    getters: {
+        getStudent: state => state.currentStudent,
+        getCompetition: state => state.currentCompetition,
+        getTask: state => state.currentTask,
+        getAllCompetitions: state => state.competitions,
+
+    },
+    mutations: {
+        setStudent: (state, studentName, studentCode) => (state.currentStudent = new CurrentStudent(studentName, studentCode)),
+        setCompetitions: (state, competitions) => (state.availableCompetitions = competitions)
+    },
+    actions: {
+        signinStudent(name, code, { commit }) {
+            commit('setStudent', name, code)
+        },
+        async fetchCompetitons({ commit }) {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            commit('setCompetitions', response)
+        }
+    },
+    modules: {}
 })
