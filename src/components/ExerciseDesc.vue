a95918dbe485497b78de7830b1a488fb36b1a811
Karl Rapur <rapurkarl@gmail.com>
2020-12-13 21:14:36 +0200

    Flexible task describtion component

--- src/components/ExerciseDesc.vue
+++ src/components/ExerciseDesc.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
-        <p>{{ task.describtion }}</p>
-        <hr>
-        <p>{{ task.task }}</p>
-        <hr>
+    <div class="flex-container">
+        <div>
+            <h2>Kirjeldus</h2>
+            <p>{{ task.describtion }}</p>
+        </div>
+        
+        
+        <div>
+            <h2>Ülesanne</h2>
+            <p>{{ task.task }}</p>
+        </div>
         <img v-if="task.media" :src="task.media" alt="Task image">
     </div>
   
@@ -17,13 +23,33 @@ export default {
 </script>
 
 <style scoped>
-div {
+
+div.flex-container {
     flex-basis: 70%;
+    display: flex;
+    flex-flow: row;
+
     margin: 10px;
     padding: 10px 20px;
     background: #f4f7f8;
     padding: 20px;
     background: #f4f7f8;
     border-radius: 8px;
+}
+@media(max-width:768px){
+  div.flex-container{
+    flex-flow: column;
+  }
+}
+
+p{
+    flex-basis: 70%;
+    padding-right: 15px;
+    text-align: justify;
+}
+img{
+    align-self: flex-start;
+    width: 30%;
+    flex-basis: 30%;
 }
 </style>
