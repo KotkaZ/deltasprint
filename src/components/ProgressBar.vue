4aa9bc4d846f978bc495ba9f3f3b8fbba4c85091
Karl Rapur <rapurkarl@gmail.com>
2020-12-14 14:00:57 +0200

    Added primitive progressbar design

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,14 +1,59 @@
 <template>
-  <div></div>
+  <div>
+    <p class="number" v-for="nr in numberOfTasks" :key="nr" :class="{ best: bestProgress == nr, my: myProgress == nr}">{{nr}}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ProgressBar"
-
+    name: "ProgressBar",
+    props: ['numberOfTasks','myProgress','bestProgress'],
+    created: function () {
+      console.log(this.bestProgress)
+    }
 }
 </script>
 
 <style scoped>
+div {
+  margin: 10px;
+  flex-basis: 100%;
+  max-width: 500px;
+  padding: 10px 20px;
+  background: #f4f7f8;
+  padding: 20px;
+  background: #f4f7f8;
+  border-radius: 8px;
 
+  display: flex;
+}
+
+div .number {
+	background: #151515;
+	color: #fff;
+	height: 30px;
+	width: 30px;
+	display: inline-block;
+	font-size: 0.8em;
+	margin-right: 4px;
+	line-height: 30px;
+	text-align: center;
+	text-shadow: 0 1px 0 rgba(255,255,255,0.2);
+	border-radius: 15px 15px 15px 0px;
+}
+
+p{
+  font-weight: bold;
+}
+
+p.best{
+	border-style: dotted;
+  border-color: #B5002F;
+  color: #151515;
+  background: #fff;
+}
+p.my{
+	background: #B79A31;
+  color: #fff;
+}
 </style>
