65daf2ef973f1780b5c96d0148feaceeb76724a2
Karl Rapur <rapurkarl@gmail.com>
2021-01-26 22:13:35 +0200

    Added accessToken to store.

--- src/store/index.js
+++ src/store/index.js
@@ -11,19 +11,22 @@ export default createStore({
         currentCompetition: null,
         currentTask: new CurrentTask("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "https://flhs.fairlawnschools.org/ourpages/auto/2020/5/26/55995212/math%20image.jpg"),
         availableCompetitions: null,
+        accessToken: null
     },
     getters: {
         getStudent: state => state.currentStudent,
         getCompetition: state => state.currentCompetition,
         getTask: state => state.currentTask,
         getCompetitions: state => state.availableCompetitions,
+        getAccessToken: state => state.accessToken
 
     },
     mutations: {
         setStudent: (state, currentStudent) => state.currentStudent = currentStudent,
         setCompetitions: (state, competitions) => state.availableCompetitions = competitions,
         setCompetition: (state, competition) => state.currentCompetition = competition,
-        setTask: (state, task) => state.currentTask = task
+        setTask: (state, task) => state.currentTask = task,
+        setAccessToken: (state, accessToken) => state.accessToken = accessToken
     },
     actions: {
         async signinStudent({ commit }, currentStudent) {
