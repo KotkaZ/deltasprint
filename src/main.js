345f53eb4799af910ac692fb95cea5695367f8b7
Karl Rapur <rapurkarl@gmail.com>
2021-02-02 23:07:28 +0200

    Added PrimeVue support.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import PrimeVue from 'primevue/config';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(PrimeVue, { ripple: true });
+app.mount('#app');
