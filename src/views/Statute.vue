ed3af7366ba974f4f13017ec373a0c514f710052
Karl Rapur <rapurkarl@gmail.com>
2021-03-18 14:38:14 +0200

    Another patch of changes to statute.

--- src/views/Statute.vue
+++ src/views/Statute.vue
@@ -1,96 +1,188 @@
 <template>
   <div class="p-d-flex p-jc-center">
     <Card class="p-col-12 p-md-8 p-shadow-12">
-      <template #header>
-        
-      </template>
+      <template #header> </template>
       <template #title>
-        <CardTitle title="DeltaSprint 2021 kevad - STATUUT"/>
+        <CardTitle title="DeltaSprint 2021 kevad - STATUUT" />
       </template>
 
       <template #content>
         <ol>
-          <li>Võistluse eesmärk on tunnustada parimaid noori loogikuid, kes on kiired ja täpsed ülesannete lahendamisel.</li>
+          <li>
+            Võistluse eesmärk on tuua kokku tudengid, kes on kiired, konkreetsed
+            ja osavad matemaatika ning infromaatika ülesannete lahendamisel.
+          </li>
 
-          <li>Võistlus kuulutatakse välja <a class="pi pi-facebook" href="https://www.facebook.com/MITS.ATI" target="_blank">MTÜ MATEMAATIKA-INFORMAATIKA TUDENGISELTS</a> Facebooki kodulehel. </li>
+          <li>
+            Võistlus kuulutatakse välja
+            <a
+              class="pi pi-facebook"
+              href="https://www.facebook.com/MITS.ATI"
+              target="_blank"
+            >
+              MTÜ MATEMAATIKA-INFORMAATIKA TUDENGISELTS</a
+            >
+            Facebooki kodulehel.
+          </li>
 
-          <li>DeltaSprint 2021 kevad (edaspidi "võistlus") auhindu annavad välja Tartu Ülikooli arvutiteaduse instituut ning Tartu Ülikooli matemaatika ja statistika instituut.</li>
+          <li>
+            DeltaSprint 2021 kevad (edaspidi "võistlus") auhindu annavad välja
+            Tartu Ülikooli arvutiteaduse instituut ning Tartu Ülikooli
+            matemaatika ja statistika instituut.
+          </li>
 
-          <li>Võistluse auhinnafond on 600 eurot, mis jaotatakse vastavalt 300€ I, 200€ II ja 100€ III koha eest.
-              Lisaks jagatakse loosi teel meeneid kõikidele lõpetanutele. Auhinnalised kohad eeldavad kõikide ülesannete lahendamist. 
-              Komisjon säilitab õiguse teha otsuseid vastavalt oma äranägemisele ja auhinnafondi mahule.</li>
+          <li>
+            Võistluse auhinnafond on 600 eurot, mis jaotatakse vastavalt 300€ I,
+            200€ II ja 100€ III koha eest. Lisaks jagatakse loosi teel meeneid
+            kõikidele lõpetanutele. Auhinnalised kohad eeldavad kõikide
+            ülesannete lahendamist. Komisjon säilitab õiguse teha otsuseid
+            vastavalt oma äranägemisele ja auhinnafondi mahule.
+          </li>
 
-          <li>Võistlusel saavad osaleda kõik loodus- ja täppisteaduste valdkonna bakalaureusetudengid.</li>
+          <li>
+            Võistlusel saavad osaleda kõik loodus- ja täppisteaduste valdkonna
+            bakalaureusetudengid.
+          </li>
 
-          <li>Võistlus toimub keskkonnas <a class="pi pi-globe" href="https://deltasprint.ee" target="_blank">DELTASPRINT</a>. Osalejatel on TUNGIVALT soovitav eelnevalt platvormiga tutvuda, osaledes mõnel avalikul katsetusel.</li>
+          <li>
+            Võistlus toimub keskkonnas
+            <a class="pi pi-globe" href="https://deltasprint.ee" target="_blank"
+              >DELTASPRINT</a
+            >. Osalejatel on TUNGIVALT soovitav eelnevalt platvormiga tutvuda,
+            osaledes mõnel avalikul katsetusel.
+          </li>
 
-          <li>Võistlusel algab ajavõtt kõikide osalejate jaoks samaaegselt. Ülesandeid saab lahendada kuni 6 tundi, mille järel ligipääs suletakse.</li>
+          <li>
+            Võistlusel algab ajavõtt kõikide osalejate jaoks samaaegselt.
+            Ülesandeid saab lahendada kuni 6 tundi, mille järel ligipääs
+            suletakse. Kui tudeng alustab võistlusega näiteks 10 minutit hiljem,
+            siis tema lõppajale liidetakse see vahe juurde.
+          </li>
 
-          <li>Ülesanded on järgnevatest kategooriatest: matemaatika, programmeerimine, algoritmid, loogika, tõenäosusteooria, krüptograafia, varia.</li>
+          <li>
+            Ülesanded on järgnevatest kategooriatest: matemaatika,
+            programmeerimine, algoritmid, loogika, tõenäosusteooria,
+            krüptograafia, <i>varia</i>.
+          </li>
 
           <li>Lahenduskäik peab olema vormistatud eesti keeles.</li>
 
           <li>
-            Lahenduse KOHUSTUSLIKUKS osaks on materjalid, mille korrektsuse eest vastutab võistleja ise.
+            Teatud ülesannete korral on lubatud kasutada välismaterjale, mille
+            hulka loetakse kõik veebist leitavad infoallikad, isiklikud
+            lahendused, õppeainete konsepktid jms. Omavahelist suhtlust
+            välismaterjalide sisse ei arvestata. Välismaterjalide kasutamine on
+            iga küsimuse juures eraldi tähistatud.
+          </li>
+
+          <li>
+            Lahenduse KOHUSTUSLIKUKS osaks on materjalid, mille korrektsuse eest
+            vastutab võistleja ise. Materjalide esitamine toimub ainult e-kirja
+            teel, juhul kui võistleja on kandideerimas auhinnalisele kohale.
             <ol>
-              <li>Lahenduse materjalide hulgas peab olema ekraanisalvestus, mis on teostatud kogu võistluse vältel. Salvestus peab hõlmama kõiki kasutusel olevaid ekraane.
-                Kohustuslik osa on ka veebikaamera pilt, mille tuge pakub lahenduskeskkond. Soovitatav tarkvara on  <a class="pi pi-globe" href="https://obsproject.com/">OBS</a>. Heli pole nõutud.
+              <li>
+                Lahenduse materjalide hulgas peab olema ekraanil toimuva
+                tegevuse salvestus, mis on teostatud kogu võistluse vältel.
+                Salvestus peab hõlmama kõiki kasutusel olevaid ekraane.
+                Kohustuslik osa on ka veebikaamera pilt, mille tuge pakub
+                lahenduskeskkond. Kaamerapilti on nähtav ainult teie arvutis ja
+                seda kuhugi ei edastata. Soovitatav tarkvara on
+                <a class="pi pi-globe" href="https://obsproject.com/">OBS</a>.
+                Heli pole nõutud.
               </li>
               <li>
-                Materjalide hulka loetakse kõik failid, mis on suuremad kui 3 MB. Lisaks on lubatud lahendada ülesandeid paberil. Sellisel juhul tuleb
+                Materjalide hulka loetakse kõik failid, mis on suuremad kui 3
+                MB. Väiksemaid faile saab esitada ülesannete juures iseseisvalt.
+                Ülesandeid on lubatud lahendada paberil. Sellisel juhul tuleb
                 lisada materjalide hulka kvaliteetsed pildid või skaneeringud.
+                Pilt võib olla teostatud telefoniga.
               </li>
               <li>
-                Kõik väikesemahulised failid, mis jäävad alla 3 MB, saab esitada ülesannete juures eraldi ühe failina.
+                Kõik väikesemahulised failid (.py, .java, .txt jms), mis jäävad
+                alla 3 MB, saab esitada ülesannete juures eraldi ühe failina.
+                Mitme faili korral pakkida need kokku .zip, .rar või mõnes
+                teises arhiivi formaadis.
               </li>
               <li>
-                Materjale peab võistleja säilitama oma andmekandjal kuni lõplike tulemuste avalikustamiseni.
+                Materjale peab võistleja säilitama oma andmekandjal kuni lõplike
+                tulemuste avalikustamiseni.
               </li>
               <li>
-                Materjalide päringu teeb komisjon võistleja e-mailile, juhul kui võistleja kandideerib auhinnalisele kohale.
+                Materjalide päringu teeb komisjon võistleja e-mailile, juhul kui
+                võistleja kandideerib auhinnalisele kohale. Iseseisvat
+                materjalide esitamist ei toimu, kuna kõiki võistlejaid üle ei
+                kontrollita.
               </li>
             </ol>
           </li>
 
-          <li>Lahenduste materjalide puudumisel võistleja auhinnalisele kohale kandideerida ei saa ning tema koht pingereas loovutatakse järgmisele osalejale.</li>
+          <li>
+            Lahenduste materjalide puudumisel võistleja auhinnalisele kohale
+            kandideerida ei saa ning tema koht pingereas loovutatakse järgmisele
+            osalejale.
+          </li>
 
-          <li>Võistluse tulemused tekivad jooksvalt võistluse käigus. Lõplikult kinnitab komisjon tulemused hiljemalt 14 päeva jooksul.</li>
+          <li>
+            Võistluse edetabel tekib jooksvalt võistluse käigus. Lõplikult
+            kinnitab komisjon tulemused hiljemalt 14 päeva jooksul.
+          </li>
 
-          <li>Lahendusi kontrollib MTÜ MATEMAATIKA-INFORMAATIKA TUDENGISELTS akadeemilise töögrupi liikmetest moodustatud komisjon. Vastuse juures kontrollitakse
-          lahenduskäiku, välismaterjalide kasutust ja üldist reeglitele vastavust.</li>
+          <li>
+            Lahendusi kontrollib MTÜ MATEMAATIKA-INFORMAATIKA TUDENGISELTS
+            akadeemilise töögrupi liikmetest moodustatud komisjon. Esituste
+            juures kontrollitakse lahenduskäiku, välismaterjalide kasutust ja
+            üldist statuudile vastavust.
+          </li>
 
-          <li>Kõikide võitjatega võetakse ühendust e-kirja teel. Oma andmete õiguse eest vastutab võistleja ise, hilisemad muudatused ei ole aktsepteeritud.</li>
+          <li>
+            Kõikide võitjatega võetakse ühendust e-kirja teel. Isikuandmete
+            õiguse eest vastutab võistleja ise, hilisemad muudatused ei ole
+            aktsepteeritud.
+          </li>
 
-          <li>Komisjon ei ole kohustatud oma otsust põhjendama ning see ei kuulu edasikaebamisele.
-          Komisjonil on õigus jätta auhinnad välja andmata, kui ei ole kvalifitseeruvaid lahendusi või esineb keskkonnaga seotud tehnilisi probleeme.</li>
-
+          <li>
+            Komisjon ei ole kohustatud oma otsust põhjendama. Komisjonil on
+            õigus jätta auhinnad välja andmata, kui ei ole kvalifitseeruvaid
+            lahendusi või esineb võistluskeskkonnaga seotud tehnilisi probleeme.
+          </li>
         </ol>
-        </template>
+      </template>
 
-        <template #footer>
-          <strong>Kontakt:</strong> Karl Rapur, DeltaSprindi korraldaja, karl.rapur@ut.ee, 55 84 366 <br>
-          Kerdo Kurs, akadeemilise töögrupi esimees, kerdo.kurs@ut.ee <br><br>
+      <template #footer>
+        <strong>Kontakt:</strong> Karl Rapur, DeltaSprindi korraldaja,
+        karl.rapur@ut.ee, 55 84 366 <br />
+        Kerdo Kurs, akadeemilise töögrupi esimees, kerdo.kurs@ut.ee <br /><br />
 
-          <strong>Toetajad:</strong><br><br>
-          <div class="p-grid p-jc-center">
-            <a href="https://www.cs.ut.ee/" target="_blank" class="p-col-12 p-md-6"><img src="../assets/ati-sinine.svg" alt="ATI logo"></a>
-            <a href="https://www.math.ut.ee/" target="_blank" class="p-col-12 p-md-6"><img src="../assets/mat-stat-sinine.svg" alt="MSI logo"></a>
-          </div>
-        </template>
+        <strong>Toetajad:</strong><br /><br />
+        <div class="p-grid p-jc-center">
+          <a
+            href="https://www.cs.ut.ee/"
+            target="_blank"
+            class="p-col-12 p-md-6"
+            ><img src="../assets/ati-sinine.svg" alt="ATI logo"
+          /></a>
+          <a
+            href="https://www.math.ut.ee/"
+            target="_blank"
+            class="p-col-12 p-md-6"
+            ><img src="../assets/mat-stat-sinine.svg" alt="MSI logo"
+          /></a>
+        </div>
+      </template>
     </Card>
   </div>
 </template>
 
 <script>
-import Card from 'primevue/card/sfc';
-import CardTitle from '../components/CardTitle'
+import Card from "primevue/card/sfc";
+import CardTitle from "../components/CardTitle";
 export default {
   name: "Statute",
   components: {
     Card,
-    CardTitle
-  }
-}
+    CardTitle,
+  },
+};
 </script>
 
 <style scoped>
@@ -110,13 +202,12 @@ li:before {
   counter-increment: item;
   font-weight: bold;
 }
-img{ 
+img {
   width: 100%;
   height: auto;
 }
 
-a{
+a {
   text-decoration: none;
 }
-
 </style>
