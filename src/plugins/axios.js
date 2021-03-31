c5443c71ea1f78b81dc858a323cce169a23661e7
Karl Rapur <rapurkarl@gmail.com>
2021-03-31 20:34:18 +0300

    Added dev api env to axios config.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,41 +2,44 @@ import axios from "axios";
 import store from "../store/index";
 
 let config = {
-    baseURL: "https://deltasprint.ee/api",
-    timeout: 60 * 1000, // Timeout
-    //withCredentials: true, // Check cross-site Access-Control
+  baseURL: process.env.VUE_APP_DEV_API || "https://deltasprint.ee/api",
+  timeout: 60 * 1000, // Timeout
+  //withCredentials: true, // Check cross-site Access-Control
 };
-
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-    function(config) {
-        // Do something before request is sent
-        return config;
-    },
-    function(error) {
-        // Do something with request error
-        return Promise.reject(error);
-    }
+  function(config) {
+    // Do something before request is sent
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-    function(response) {
-        // Do something with response data
+  function(response) {
+    // Do something with response data
 
-        return response;
-    },
-    function(error) {
-        // Do something with response error
-        if (error.response && error.response.status === 403 && error.response.config && !error.config._isRetryRequest) {
-            store.dispatch('removeHeaders');
-        }
-
-        return Promise.reject(error);
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    if (
+      error.response &&
+      error.response.status === 403 &&
+      error.response.config &&
+      !error.config._isRetryRequest
+    ) {
+      store.dispatch("removeHeaders");
     }
-);
 
+    return Promise.reject(error);
+  }
+);
 
 export default _axios;
